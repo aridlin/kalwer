456e520c149fc9a71ea6ef3574e80808cc901d83
Igor Zwierzchowski <telopeigor@gmail.com>
2026-09-18 18:52:32 +0200

    Release Kalwer 0.11.0 native search modes

--- windows/kalwer_windows.cpp
+++ windows/kalwer_windows.cpp
@@ -86,7 +86,7 @@ constexpr UINT kCommandChangedMessage = WM_APP + 42;
 constexpr UINT kCloseAdminPopupMessage = WM_APP + 45;
 constexpr wchar_t kAdminWindowTitle[] = L"Kalwer Administrator PTY";
 constexpr float kCloseDurationMs = 280.0f;
-constexpr wchar_t kKalwerVersion[] = L"0.10.0";
+constexpr wchar_t kKalwerVersion[] = L"0.11.0";
 constexpr wchar_t kLatestReleaseUrl[] =
     L"https://github.com/aridlin/kalwer/releases/latest";
 
